55fc2e55fe0ea15f932a7ec916cdb461c04662a5
Sofia <cheery_sonya@mail.ru>
2022-07-23 17:38:02 +0300

    =создает список символов

--- main.py
+++ main.py
@@ -76,100 +76,7 @@ def polynom_with_boundary_conditions(x0,y0,x1,y1):
     return a0,a1,a2,a3
 
 
-#
-def spline(x1,x2,x3,x4,x5,y1,y2,y3,y4,y5):
-    x=sympy.Symbol('x')
-    a0,a1,a2,a3,b0,b1,b2,b3,c0,c1,c2,c3,d0,d1,d2,d3=sympy.symbols('a0,a1,a2,a3,b0,b1,b2,b3,c0,c1,c2,c3,d0,d1,d2,d3')
 
-    nscA=a0+a1*x+a2*x**2+a3*x**3
-    nscB=b0+b1*x+b2*x**2+b3*x**3
-    nscC=c0+c1*x+c2*x**2+c3*x**3
-    nscD=d0+d1*x+d2*x**2+d3*x**3
-
-    #sol=sympy.solve([a0*x1-1,b0*x2-3],[a0,b0])
-    sol2=sympy.solve([nscA.subs(x,x1)-y1,
-                      diff(nscA,x).subs(x,x1),
-                      nscA.subs(x,x2)-y2,
-                      nscB.subs(x, x2) - y2,
-                      diff(nscA,x).subs(x,x2)-diff(nscB,x).subs(x,x2),
-                      diff(nscA,x,2).subs(x,x2)-diff(nscB,x,2).subs(x,x2),
-
-                      nscB.subs(x, x3) - y3,
-                      nscC.subs(x, x3) - y3,
-                      diff(nscB, x).subs(x, x3) - diff(nscC, x).subs(x, x3),
-                      diff(nscB, x, 2).subs(x, x3) - diff(nscC, x, 2).subs(x, x3),
-
-                      nscC.subs(x, x4) - y4,
-                      nscD.subs(x, x4) - y4,
-                      diff(nscC, x).subs(x, x4) - diff(nscD, x).subs(x, x4),
-                      diff(nscC, x, 2).subs(x, x4) - diff(nscD, x, 2).subs(x, x4),
-
-                      nscD.subs(x, x5) - y5,
-                      diff(nscD, x).subs(x, x5)
-                      ],[a0,a1,a2,a3,b0,b1,b2,b3,c0,c1,c2,c3,d0,d1,d2,d3])
-    #print('so',sol)
-    print('sol2',sol2)
-    print('sol2a0', sol2[a0])
-    a0=sol2[a0]
-    a1 = sol2[a1]
-    a2 = sol2[a2]
-    a3 = sol2[a3]
-
-    b0 = sol2[b0]
-    b1 = sol2[b1]
-    b2 = sol2[b2]
-    b3 = sol2[b3]
-
-    c0 = sol2[c0]
-    c1 = sol2[c1]
-    c2 = sol2[c2]
-    c3 = sol2[c3]
-
-    d0 = sol2[d0]
-    d1 = sol2[d1]
-    d2 = sol2[d2]
-    d3 = sol2[d3]
-    original_stdout = sys.stdout
-    FileGraphMatrix = open('Coefficients', 'w')
-    sys.stdout = FileGraphMatrix
-
-    print('a0=')
-    print(a0)#+ '\n')
-    print('a1=')
-    print(a1)#+ '\n') + '\n')
-    print('a2=')
-    print(a2)# + '\n')
-    print('a3=')
-    print( a3 )#+ '\n')
-    print('b0=')
-    print( b0 )#+ '\n')
-    print('b1=')
-    print( b1 )#+ '\n')
-    print('b2=')
-    print( b2 )#+ '\n')
-    print('b3=')
-    print( b3 )#+ '\n')
-    print('c0=')
-    print( c0 )#+ '\n')
-    print('c1=')
-    print( c1 )#+ '\n')
-    print('c2=')
-    print( c2 )#+ '\n')
-    print('c3=')
-    print( c3 )#+ '\n')
-    print('d0=')
-    print( d0 )#+ '\n')
-    print('d1=')
-    print( d1 )#+ '\n')
-    print('d2=')
-    print( d2 )#+ '\n')
-    print('d3=')
-    print( d3 )#+ '\n')
-
-    sys.stdout = original_stdout
-    FileGraphMatrix.close()
-
-    return a0,a1,a2,a3,b0,b1,b2,b3,c0,c1,c2,c3,d0,d1,d2,d3
 
 #процедура составляющая список уравнений для сплайна по массивам точек X,Y points(x[i],y[i])
 def list_of_equations(X,Y,nsc):
@@ -197,7 +104,7 @@ def list_of_equations(X,Y,nsc):
     return eq
 
 
-def spline_new(x1,x2,x3,x4,x5,y1,y2,y3,y4,y5):
+def spline(x1,x2,x3,x4,x5,y1,y2,y3,y4,y5):
     x=sympy.Symbol('x')
     a0,a1,a2,a3,b0,b1,b2,b3,c0,c1,c2,c3,d0,d1,d2,d3=sympy.symbols('a0,a1,a2,a3,b0,b1,b2,b3,c0,c1,c2,c3,d0,d1,d2,d3')
 
@@ -213,8 +120,8 @@ def spline_new(x1,x2,x3,x4,x5,y1,y2,y3,y4,y5):
 
     sol2=sympy.solve(eq,[a0,a1,a2,a3,b0,b1,b2,b3,c0,c1,c2,c3,d0,d1,d2,d3])
     #print('so',sol)
-    print('sol2',sol2)
-    print('sol2a0', sol2[a0])
+    #print('sol2',sol2)
+    #print('sol2a0', sol2[a0])
     a0=sol2[a0]
     a1 = sol2[a1]
     a2 = sol2[a2]
@@ -287,7 +194,30 @@ def long_subs(a0, a1, a2, a3, b0, b1, b2, b3, c0, c1, c2, c3, d0, d1, d2, d3,
     return a0, a1, a2, a3, b0, b1, b2, b3, c0, c1, c2, c3, d0, d1, d2, d3
 
 
+def draw_pieswize_f(a0,a1,a2,a3,b0,b1,b2,b3,c0,c1,c2,c3,d0,d1,d2,d3,x11,x22,x33,x44,x55):
+    x = sympy.Symbol('x')
+    p=[None]*4
+    #p=[1,2,3,4]
+    #p.append(1)
+    p[0] = plot(a0 + a1 * x + a2 * x ** 2 + a3 * x ** 3, (x, x11, x22), show=False)
+    p[0].line_color = 'cyan'
+    p[1] = plot(b0 + b1 * x + b2 * x ** 2 + b3 * x ** 3, (x, x22, x33), show=False)
+    p[1].line_color = 'red'
+    p[2] = plot(c0 + c1 * x + c2 * x ** 2 + c3 * x ** 3, (x, x33, x44), show=False, line_color='red')
+    p[3] = plot(d0 + d1 * x + d2 * x ** 2 + d3 * x ** 3, (x, x44, x55), show=False, line_color='blue')
 
+    p[0].extend(p[1])
+    p[0].extend(p[2])
+    p[0].extend(p[3])
+    p[0].show()
+
+#процедура, создающая список символьных переменных
+def list_of_symbols(n):
+    list=[None]*n
+    for i in range(n):
+        stri='x'+str(i)
+        list[i]=stri
+    return list
 
 def main():
    #print('hello')
@@ -310,7 +240,7 @@ def main():
 
 
    a0, a1, a2, a3, b0, b1, b2, b3, c0, c1, c2, c3, d0, d1, d2, d3=sympy.symbols(' a0, a1, a2, a3, b0, b1, b2, b3, c0, c1, c2, c3, d0, d1, d2, d3')
-   a0,a1,a2,a3,b0,b1,b2,b3,c0,c1,c2,c3,d0,d1,d2,d3=spline_new(x1,x2,x3,x4,x5,y1,y2,y3,y4,y5)
+   a0,a1,a2,a3,b0,b1,b2,b3,c0,c1,c2,c3,d0,d1,d2,d3=spline(x1,x2,x3,x4,x5,y1,y2,y3,y4,y5)
 
    #a0, a1, a2, a3 = polynom_with_boundary_conditions(x1, y1, x2, y2)
    #b0, b1, b2, b3 = polynom_with_boundary_conditions(x2, y2, x3, y3)
@@ -377,18 +307,22 @@ def main():
    ##plot(a0 + a1 * x+a2*x**2+a3*x**3,b0 + b1 * x+b2*x**2+b3*x**3,
        # c0 + c1 * x+c2*x**2+c3*x**3,d0 + d1 * x+d2*x**2+d3*x**3,(x,x11,x55),ylim=(-2,5))
    ##plot(a0+1 + a1 * x + a2 * x ** 2, (x, 0, 1))
-   p1=   plot(a0 + a1 * x+a2*x**2+a3*x**3,(x,x11,x22),show=False)
-   p1.line_color='cyan'
-   p2 = plot(b0 + b1 * x + b2 * x ** 2 + b3 * x ** 3, (x, x22, x33), show=False)
-   p2.line_color = 'red'
-   p3 = plot(c0 + c1 * x + c2 * x ** 2 + c3 * x ** 3, (x, x33, x44), show=False,line_color='red')
-   p4 = plot(d0 + d1 * x + d2 * x ** 2 + d3 * x ** 3, (x, x44, x55), show=False,line_color='blue')
 
-   p1.extend(p2)
-   p1.extend(p3)
-   p1.extend(p4)
-   p1.show()
+   draw_pieswize_f(a0, a1, a2, a3, b0, b1, b2, b3, c0, c1, c2, c3, d0, d1, d2, d3, x11,x22, x33, x44, x55 )
+   #p1=   plot(a0 + a1 * x+a2*x**2+a3*x**3,(x,x11,x22),show=False)
+  # p1.line_color='cyan'
+  # p2 = plot(b0 + b1 * x + b2 * x ** 2 + b3 * x ** 3, (x, x22, x33), show=False)
+  # p2.line_color = 'red'
+  # p3 = plot(c0 + c1 * x + c2 * x ** 2 + c3 * x ** 3, (x, x33, x44), show=False,line_color='red')
+  # p4 = plot(d0 + d1 * x + d2 * x ** 2 + d3 * x ** 3, (x, x44, x55), show=False,line_color='blue')
 
+   #p1.extend(p2)
+  # p1.extend(p3)
+  # p1.extend(p4)
+  # p1.show()
+
+   list_o=list_of_symbols(5)
+   print(list_o)
 
 
 # Press the green button in the gutter to run the script.
